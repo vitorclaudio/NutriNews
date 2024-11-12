80ac64e27f981dfa0d6ca554d84b286ace163cbf
vitorclaudio <vitorcldeosantos@gmail.com>
2024-11-12 00:15:59 -0300

    Mudança na rota /NutriNews no index.js

--- nutrinews/src/apiCalls.js
+++
@@ -1,9 +0,0 @@
-const apiCalls = {
-  getTopStories() {
-    const apiKey = process.env.REACT_APP_NYT_TECH_CX
-    return fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${apiKey}`)
-      .then(res => res.json())
-  }
-}
-
-export default apiCalls;
